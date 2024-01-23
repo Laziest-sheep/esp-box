7339da764dc1604006bcd7d8bc650af347bfa378
grazie.qu <11037grazie@gmail.com>
2024-01-24 00:10:32 +0800

    基本界面完成

--- examples/image_display/main/my_lvgl.c
+++ examples/image_display/main/my_lvgl.c
@@ -1,5 +1,7 @@
 #include "my_lvgl.h"
 #include "lvgl.h"
+#include "bsp/esp-bsp.h"
+#include "esp_log.h"
 
 
 #define PROFILE_SIZE        150
@@ -14,10 +16,30 @@
 
 
 LV_IMG_DECLARE(profile);
+LV_IMG_DECLARE(led_img);
+LV_IMG_DECLARE(AC_img);
+LV_IMG_DECLARE(SR_img);
+LV_IMG_DECLARE(listen_img);
 static lv_obj_t *scr_start;
 static lv_obj_t *scr_func;
+static lv_obj_t *scr_listen;
+static lv_obj_t *scr_respond;
 static lv_obj_t *btn_start;
 static lv_obj_t *label_start;
+static lv_obj_t *switch_led;
+static lv_obj_t *switch_AC;     //air-conditioning
+static lv_obj_t *switch_SR;     //speech recognition
+static lv_obj_t *img_profile;
+static lv_obj_t *img_led;
+static lv_obj_t *img_SR;
+static lv_obj_t *img_AC;
+static lv_obj_t *img_listen;
+static lv_obj_t *obj_led_bg;
+static lv_obj_t *obj_AC_bg;
+static lv_obj_t *obj_SR_bg;
+static lv_obj_t *label_led;
+static lv_obj_t *label_AC;
+static lv_obj_t *label_SR;
 static lv_font_t *font = (lv_font_t *)&lv_font_montserrat_12;
 
 
@@ -30,24 +52,81 @@ static void btn_start_cb(lv_event_t *e)
     }
 }
 
-void screen_init(void)
+static void switch_led_cb(lv_event_t *e)
 {
-    /*创建两个页面，初始页面和功能页面*/
+    if(lv_obj_has_state(switch_led, LV_STATE_CHECKED))
+    {
+        
+    }
+    else
+    {
+        
+    }
+}
+
+static void switch_AC_cb(lv_event_t *e)
+{
+    if(lv_obj_has_state(switch_AC, LV_STATE_CHECKED))
+    {
+        
+    }
+    else
+    {
+        
+    }
+}
+
+static void switch_SR_cb(lv_event_t *e)
+{
+    if(lv_obj_has_state(switch_SR, LV_STATE_CHECKED))
+    {
+        
+    }
+    else
+    {
+        
+    }
+}
+
+static void scr_listen_init(void)
+{
+    img_listen = lv_img_create(scr_listen);
+    lv_img_set_src(img_listen, &listen_img);
+    lv_img_set_zoom(img_listen, 128);
+    lv_img_set_pivot(img_listen, 200, 0);
+}
+
+static void scr_respond_init(void)
+{
+
+}
+
+
+static void scr_module_init(void)
+{
+    /*创建页面*/
     scr_start = lv_obj_create(NULL);
     scr_func = lv_obj_create(NULL);
+    scr_listen = lv_obj_create(NULL);
+    scr_respond = lv_obj_create(NULL);
 
     /*禁用页面滚动*/
     lv_obj_clear_flag(scr_start, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_clear_flag(scr_func, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_clear_flag(scr_listen, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_clear_flag(scr_respond, LV_OBJ_FLAG_SCROLLABLE);
     
-    /*设置白色背景*/
+    /*设置背景颜色*/
     lv_obj_set_style_bg_color(scr_start, lv_color_hex(0xffffff), LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(scr_func, lv_color_hex(0x00ffff), LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(scr_listen, lv_color_hex(0xffffff), LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(scr_respond, lv_color_hex(0xffffff), LV_STATE_DEFAULT);
 
     /*加载初始页面*/
     lv_scr_load(scr_start);
 
     /*初始页面图像显示*/
-    lv_obj_t *img_profile = lv_img_create(scr_start);
+    img_profile = lv_img_create(scr_start);
     lv_img_set_src(img_profile, &profile);
     lv_img_set_zoom(img_profile, PROFILE_SIZE);
     lv_img_set_pivot(img_profile, PROFILE_POS_X, PROFILE_POS_Y);
@@ -64,4 +143,102 @@ void screen_init(void)
     lv_obj_set_style_text_font(label_start, font, LV_PART_MAIN);
     lv_label_set_text(label_start, "START WORK");
     lv_obj_set_align(label_start, LV_ALIGN_CENTER);
+
+    /*语音识别界面*/
+    scr_listen_init();
+
+    /*回复界面*/
+    scr_respond_init();
+}
+
+static void led_module_init(void)
+{
+    obj_led_bg = lv_obj_create(scr_func);
+    lv_obj_clear_flag(obj_led_bg, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_size(obj_led_bg, lv_obj_get_height(scr_func) / 3, lv_obj_get_height(scr_func) / 2);
+    lv_obj_align(obj_led_bg, LV_ALIGN_CENTER, -100, 0);
+
+    label_led = lv_label_create(obj_led_bg);
+    lv_label_set_text(label_led, "LED");
+    lv_obj_set_style_text_font(label_led, font, LV_STATE_DEFAULT);
+    lv_obj_align(label_led, LV_ALIGN_CENTER, 0, 15);
+
+    switch_led = lv_switch_create(obj_led_bg);
+    lv_obj_set_size(switch_led, lv_obj_get_height(scr_func) / 6, lv_obj_get_height(scr_func) / 12);
+    lv_obj_align(switch_led, LV_ALIGN_CENTER, 0, 40);
+    lv_obj_add_event_cb(switch_led, switch_led_cb, LV_EVENT_VALUE_CHANGED, NULL);
+
+    img_led = lv_img_create(obj_led_bg);
+    lv_img_set_src(img_led, &led_img);
+    lv_img_set_zoom(img_led, 50);
+    lv_img_set_pivot(img_led, 8, 0);
+}
+
+static void AC_module_init(void)
+{
+    obj_AC_bg = lv_obj_create(scr_func);
+    lv_obj_clear_flag(obj_AC_bg, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_size(obj_AC_bg, lv_obj_get_height(scr_func) / 3, lv_obj_get_height(scr_func) / 2);
+    lv_obj_align(obj_AC_bg, LV_ALIGN_CENTER, 0, 0);
+
+    label_AC = lv_label_create(obj_AC_bg);
+    lv_label_set_text(label_AC, "AC");
+    lv_obj_set_style_text_font(label_AC, font, LV_STATE_DEFAULT);
+    lv_obj_align(label_AC, LV_ALIGN_CENTER, 0, 15);
+
+    switch_AC = lv_switch_create(obj_AC_bg);
+    lv_obj_set_size(switch_AC, lv_obj_get_height(scr_func) / 6, lv_obj_get_height(scr_func) / 12);
+    lv_obj_align(switch_AC, LV_ALIGN_CENTER, 0, 40);
+    lv_obj_add_event_cb(switch_AC, switch_AC_cb, LV_EVENT_VALUE_CHANGED, NULL);
+
+    img_AC = lv_img_create(obj_AC_bg);
+    lv_img_set_src(img_AC, &AC_img);
+    lv_img_set_zoom(img_AC, 50);
+    lv_img_set_pivot(img_AC, 8, 0);
+}
+
+static void SR_module_init(void)
+{
+    obj_SR_bg = lv_obj_create(scr_func);
+    lv_obj_clear_flag(obj_SR_bg, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_size(obj_SR_bg, lv_obj_get_height(scr_func) / 3, lv_obj_get_height(scr_func) / 2);
+    lv_obj_align(obj_SR_bg, LV_ALIGN_CENTER, 100, 0);
+
+    label_SR = lv_label_create(obj_SR_bg);
+    lv_label_set_text(label_SR, "SR");
+    lv_obj_set_style_text_font(label_SR, font, LV_STATE_DEFAULT);
+    lv_obj_align(label_SR, LV_ALIGN_CENTER, 0, 15);
+
+    switch_SR = lv_switch_create(obj_SR_bg);
+    lv_obj_add_state(switch_SR, LV_STATE_CHECKED);
+    lv_obj_set_size(switch_SR, lv_obj_get_height(scr_func) / 6, lv_obj_get_height(scr_func) / 12);
+    lv_obj_align(switch_SR, LV_ALIGN_CENTER, 0, 40);
+    lv_obj_add_event_cb(switch_SR, switch_SR_cb, LV_EVENT_VALUE_CHANGED, NULL);
+
+    img_SR = lv_img_create(obj_SR_bg);
+    lv_img_set_src(img_SR, &SR_img);
+    lv_img_set_zoom(img_SR, 50);
+    lv_img_set_pivot(img_SR, 8, 0);
+}
+
+void screen_init(void)
+{
+    /*页面创建*/
+    scr_module_init();
+    
+    /*电灯开关*/
+    led_module_init();
+
+    /*空调开关*/
+    AC_module_init();
+
+    /*语音识别开关*/
+    SR_module_init();
+
+    //TODO
+    /*语音识别界面*/
+    // lv_scr_load(scr_listen);
+
+    //TODO
+    /*答复界面*/
 }
