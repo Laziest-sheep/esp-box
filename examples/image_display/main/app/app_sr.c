ecff28726f9066cfd77df3b5892f51659733c821
grazie.qu <11037grazie@gmail.com>
2024-02-20 01:03:54 +0800

    修改时钟频率,初始音量

--- examples/image_display/main/app/app_sr.c
+++ examples/image_display/main/app/app_sr.c
@@ -70,49 +70,49 @@ static sr_data_t *g_sr_data = NULL;
 /**
  * @brief all default commands
  */
-// static const sr_cmd_t g_default_cmd_info[] = {
-//     // // English
-//     {SR_CMD_LIGHT_ON, SR_LANG_EN, 0, "Turn On the Light", "TkN nN jc LiT", {NULL}},
-//     {SR_CMD_LIGHT_ON, SR_LANG_EN, 0, "Switch On the Light", "SWgp nN jc LiT", {NULL}},
-//     {SR_CMD_LIGHT_OFF, SR_LANG_EN, 0, "Switch Off the Light", "SWgp eF jc LiT", {NULL}},
-//     {SR_CMD_LIGHT_OFF, SR_LANG_EN, 0, "Turn Off the Light", "TkN eF jc LiT", {NULL}},
-//     {SR_CMD_SET_RED, SR_LANG_EN, 0, "Turn Red", "TkN RfD", {NULL}},
-//     {SR_CMD_SET_GREEN, SR_LANG_EN, 0, "Turn Green", "TkN GRmN", {NULL}},
-//     {SR_CMD_SET_BLUE, SR_LANG_EN, 0, "Turn Blue", "TkN BLo", {NULL}},
-//     {SR_CMD_CUSTOMIZE_COLOR, SR_LANG_EN, 0, "Customize Color", "KcSTcMiZ KcLk", {NULL}},
-//     {SR_CMD_PLAY, SR_LANG_EN, 0, "Sing a song", "Sgl c Sel", {NULL}},
-//     {SR_CMD_PLAY, SR_LANG_EN, 0, "Play Music", "PLd MYoZgK", {NULL}},
-//     {SR_CMD_NEXT, SR_LANG_EN, 0, "Next Song", "NfKST Sel", {NULL}},
-//     {SR_CMD_PAUSE, SR_LANG_EN, 0, "Pause Playing", "PeZ PLdgl", {NULL}},
+static const sr_cmd_t g_default_cmd_info[] = {
+    // // English
+    {SR_CMD_LIGHT_ON, SR_LANG_EN, 0, "Turn On the Light", "TkN nN jc LiT", {NULL}},
+    {SR_CMD_LIGHT_ON, SR_LANG_EN, 0, "Switch On the Light", "SWgp nN jc LiT", {NULL}},
+    {SR_CMD_LIGHT_OFF, SR_LANG_EN, 0, "Switch Off the Light", "SWgp eF jc LiT", {NULL}},
+    {SR_CMD_LIGHT_OFF, SR_LANG_EN, 0, "Turn Off the Light", "TkN eF jc LiT", {NULL}},
+    {SR_CMD_SET_RED, SR_LANG_EN, 0, "Turn Red", "TkN RfD", {NULL}},
+    {SR_CMD_SET_GREEN, SR_LANG_EN, 0, "Turn Green", "TkN GRmN", {NULL}},
+    {SR_CMD_SET_BLUE, SR_LANG_EN, 0, "Turn Blue", "TkN BLo", {NULL}},
+    {SR_CMD_CUSTOMIZE_COLOR, SR_LANG_EN, 0, "Customize Color", "KcSTcMiZ KcLk", {NULL}},
+    {SR_CMD_PLAY, SR_LANG_EN, 0, "Sing a song", "Sgl c Sel", {NULL}},
+    {SR_CMD_PLAY, SR_LANG_EN, 0, "Play Music", "PLd MYoZgK", {NULL}},
+    {SR_CMD_NEXT, SR_LANG_EN, 0, "Next Song", "NfKST Sel", {NULL}},
+    {SR_CMD_PAUSE, SR_LANG_EN, 0, "Pause Playing", "PeZ PLdgl", {NULL}},
 
-//     {SR_CMD_AC_ON, SR_LANG_EN, 0, "Turn on the Air", "TkN nN jc fR", {NULL}},
-//     {SR_CMD_AC_OFF, SR_LANG_EN, 0, "Turn off the Air", "TkN eF jc fR", {NULL}},
+    {SR_CMD_AC_ON, SR_LANG_EN, 0, "Turn on the Air", "TkN nN jc fR", {NULL}},
+    {SR_CMD_AC_OFF, SR_LANG_EN, 0, "Turn off the Air", "TkN eF jc fR", {NULL}},
 
-//     // Chinese
-//     {SR_CMD_LIGHT_ON, SR_LANG_CN, 0, "打开电灯", "da kai dian deng", {NULL}},
-//     {SR_CMD_LIGHT_OFF, SR_LANG_CN, 0, "关闭电灯", "guan bi dian deng", {NULL}},
-//     {SR_CMD_SET_RED, SR_LANG_CN, 0, "调成红色", "tiao cheng hong se", {NULL}},
-//     {SR_CMD_SET_GREEN, SR_LANG_CN, 0, "调成绿色", "tiao cheng lv se", {NULL}},
-//     {SR_CMD_SET_BLUE, SR_LANG_CN, 0, "调成蓝色", "tiao cheng lan se", {NULL}},
-//     {SR_CMD_CUSTOMIZE_COLOR, SR_LANG_CN, 0, "自定义颜色", "zi ding yi yan se", {NULL}},
-//     {SR_CMD_PLAY, SR_LANG_CN, 0, "播放音乐", "bo fang yin yue", {NULL}},
-//     {SR_CMD_NEXT, SR_LANG_CN, 0, "切歌", "qie ge", {NULL}},
-//     {SR_CMD_NEXT, SR_LANG_CN, 0, "下一曲", "xia yi qu", {NULL}},
-//     {SR_CMD_PAUSE, SR_LANG_CN, 0, "暂停", "zan ting", {NULL}},
-//     {SR_CMD_PAUSE, SR_LANG_CN, 0, "暂停播放", "zan ting bo fang", {NULL}},
-//     {SR_CMD_PAUSE, SR_LANG_CN, 0, "停止播放", "ting zhi bo fang", {NULL}},
+    // Chinese
+    {SR_CMD_LIGHT_ON, SR_LANG_CN, 0, "打开电灯", "da kai dian deng", {NULL}},
+    {SR_CMD_LIGHT_OFF, SR_LANG_CN, 0, "关闭电灯", "guan bi dian deng", {NULL}},
+    {SR_CMD_SET_RED, SR_LANG_CN, 0, "调成红色", "tiao cheng hong se", {NULL}},
+    {SR_CMD_SET_GREEN, SR_LANG_CN, 0, "调成绿色", "tiao cheng lv se", {NULL}},
+    {SR_CMD_SET_BLUE, SR_LANG_CN, 0, "调成蓝色", "tiao cheng lan se", {NULL}},
+    {SR_CMD_CUSTOMIZE_COLOR, SR_LANG_CN, 0, "自定义颜色", "zi ding yi yan se", {NULL}},
+    {SR_CMD_PLAY, SR_LANG_CN, 0, "播放音乐", "bo fang yin yue", {NULL}},
+    {SR_CMD_NEXT, SR_LANG_CN, 0, "切歌", "qie ge", {NULL}},
+    {SR_CMD_NEXT, SR_LANG_CN, 0, "下一曲", "xia yi qu", {NULL}},
+    {SR_CMD_PAUSE, SR_LANG_CN, 0, "暂停", "zan ting", {NULL}},
+    {SR_CMD_PAUSE, SR_LANG_CN, 0, "暂停播放", "zan ting bo fang", {NULL}},
+    {SR_CMD_PAUSE, SR_LANG_CN, 0, "停止播放", "ting zhi bo fang", {NULL}},
 
-//     {SR_CMD_AC_ON, SR_LANG_CN, 0, "打开空调", "da kai kong tiao", {NULL}},
-//     {SR_CMD_AC_OFF, SR_LANG_CN, 0, "关闭空调", "guan bi kong tiao", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "舒适模式", "shu shi mo shi", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "制冷模式", "zhi leng mo shi", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "制热模式", "zhi re mo shi", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "加热模式", "jia re mo shi", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "除湿模式", "chu shi mo shi", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "送风模式", "song feng mo shi", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "升高温度", "sheng gao wen du", {NULL}},
-//     {SR_CMD_MAX, SR_LANG_CN, 0, "降低温度", "jiang di wen du", {NULL}},
-// };
+    {SR_CMD_AC_ON, SR_LANG_CN, 0, "打开空调", "da kai kong tiao", {NULL}},
+    {SR_CMD_AC_OFF, SR_LANG_CN, 0, "关闭空调", "guan bi kong tiao", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "舒适模式", "shu shi mo shi", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "制冷模式", "zhi leng mo shi", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "制热模式", "zhi re mo shi", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "加热模式", "jia re mo shi", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "除湿模式", "chu shi mo shi", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "送风模式", "song feng mo shi", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "升高温度", "sheng gao wen du", {NULL}},
+    {SR_CMD_MAX, SR_LANG_CN, 0, "降低温度", "jiang di wen du", {NULL}},
+};
 
 static void audio_feed_task(void *arg)
 {
@@ -272,120 +272,120 @@ static void audio_detect_task(void *arg)
 
 esp_err_t app_sr_set_language(sr_language_t new_lang)
 {
-    // ESP_RETURN_ON_FALSE(NULL != g_sr_data, ESP_ERR_INVALID_STATE, TAG, "SR is not running");
+    ESP_RETURN_ON_FALSE(NULL != g_sr_data, ESP_ERR_INVALID_STATE, TAG, "SR is not running");
 
-    // if (new_lang == g_sr_data->lang) {
-    //     ESP_LOGW(TAG, "nothing to do");
-    //     return ESP_OK;
-    // } else {
-    //     g_sr_data->lang = new_lang;
-    // }
+    if (new_lang == g_sr_data->lang) {
+        ESP_LOGW(TAG, "nothing to do");
+        return ESP_OK;
+    } else {
+        g_sr_data->lang = new_lang;
+    }
 
-    // ESP_LOGW(TAG, "Set language to %s", SR_LANG_EN == g_sr_data->lang ? "EN" : "CN");
-    // if (g_sr_data->model_data) {
-    //     g_sr_data->multinet->destroy(g_sr_data->model_data);
-    // }
+    ESP_LOGW(TAG, "Set language to %s", SR_LANG_EN == g_sr_data->lang ? "EN" : "CN");
+    if (g_sr_data->model_data) {
+        g_sr_data->multinet->destroy(g_sr_data->model_data);
+    }
 
-    // g_sr_data->cmd_num = 0;
+    g_sr_data->cmd_num = 0;
 
-    // char *wn_name = esp_srmodel_filter(models, ESP_WN_PREFIX, (SR_LANG_EN == g_sr_data->lang ? "hiesp" : "hilexin"));
-    // ESP_RETURN_ON_FALSE(NULL != wn_name, ESP_ERR_INVALID_ARG, TAG, "Modifications to the code are required to support the relevant configuration");
-    // g_sr_data->afe_handle->set_wakenet(g_sr_data->afe_data, wn_name);
-    // ESP_LOGI(TAG, "load wakenet:%s", wn_name);
+    char *wn_name = esp_srmodel_filter(models, ESP_WN_PREFIX, (SR_LANG_EN == g_sr_data->lang ? "hiesp" : "hilexin"));
+    ESP_RETURN_ON_FALSE(NULL != wn_name, ESP_ERR_INVALID_ARG, TAG, "Modifications to the code are required to support the relevant configuration");
+    g_sr_data->afe_handle->set_wakenet(g_sr_data->afe_data, wn_name);
+    ESP_LOGI(TAG, "load wakenet:%s", wn_name);
 
-    // char *mn_name = esp_srmodel_filter(models, ESP_MN_PREFIX, ((SR_LANG_EN == g_sr_data->lang) ? ESP_MN_ENGLISH : ESP_MN_CHINESE));
-    // ESP_RETURN_ON_FALSE(NULL != mn_name, ESP_ERR_INVALID_ARG, TAG, "Modifications to the code are required to support the relevant configuration");
-    // esp_mn_iface_t *multinet = esp_mn_handle_from_name(mn_name);
-    // model_iface_data_t *model_data = multinet->create(mn_name, 5760);
-    // g_sr_data->multinet = multinet;
-    // g_sr_data->model_data = model_data;
-    // g_sr_data->mn_name = mn_name;
-    // ESP_LOGI(TAG, "load multinet:%s", g_sr_data->mn_name);
+    char *mn_name = esp_srmodel_filter(models, ESP_MN_PREFIX, ((SR_LANG_EN == g_sr_data->lang) ? ESP_MN_ENGLISH : ESP_MN_CHINESE));
+    ESP_RETURN_ON_FALSE(NULL != mn_name, ESP_ERR_INVALID_ARG, TAG, "Modifications to the code are required to support the relevant configuration");
+    esp_mn_iface_t *multinet = esp_mn_handle_from_name(mn_name);
+    model_iface_data_t *model_data = multinet->create(mn_name, 5760);
+    g_sr_data->multinet = multinet;
+    g_sr_data->model_data = model_data;
+    g_sr_data->mn_name = mn_name;
+    ESP_LOGI(TAG, "load multinet:%s", g_sr_data->mn_name);
 
-    // // remove all command
-    // app_sr_remove_all_cmd();
-    // if (strstr(g_sr_data->mn_name, "mn6")) {
-    //     esp_mn_commands_clear();
-    // }
+    // remove all command
+    app_sr_remove_all_cmd();
+    if (strstr(g_sr_data->mn_name, "mn6")) {
+        esp_mn_commands_clear();
+    }
 
-    // uint8_t cmd_number = 0;
-    // // count command number
-    // for (size_t i = 0; i < sizeof(g_default_cmd_info) / sizeof(sr_cmd_t); i++) {
-    //     if (g_default_cmd_info[i].lang == g_sr_data->lang) {
-    //         app_sr_add_cmd(&g_default_cmd_info[i]);
-    //         cmd_number++;
-    //     }
-    // }
-    // ESP_LOGI(TAG, "cmd_number=%d", cmd_number);
+    uint8_t cmd_number = 0;
+    // count command number
+    for (size_t i = 0; i < sizeof(g_default_cmd_info) / sizeof(sr_cmd_t); i++) {
+        if (g_default_cmd_info[i].lang == g_sr_data->lang) {
+            app_sr_add_cmd(&g_default_cmd_info[i]);
+            cmd_number++;
+        }
+    }
+    ESP_LOGI(TAG, "cmd_number=%d", cmd_number);
 
-    // return app_sr_update_cmds();/* Reset command list */
+    return app_sr_update_cmds();/* Reset command list */
     return ESP_OK;
 }
 
 esp_err_t app_sr_start(bool record_en)
 {
-//     esp_err_t ret = ESP_OK;
-//     ESP_RETURN_ON_FALSE(NULL == g_sr_data, ESP_ERR_INVALID_STATE, TAG, "SR already running");
+    esp_err_t ret = ESP_OK;
+    ESP_RETURN_ON_FALSE(NULL == g_sr_data, ESP_ERR_INVALID_STATE, TAG, "SR already running");
 
-//     g_sr_data = heap_caps_calloc(1, sizeof(sr_data_t), MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-//     ESP_RETURN_ON_FALSE(NULL != g_sr_data, ESP_ERR_NO_MEM, TAG, "Failed create sr data");
+    g_sr_data = heap_caps_calloc(1, sizeof(sr_data_t), MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+    ESP_RETURN_ON_FALSE(NULL != g_sr_data, ESP_ERR_NO_MEM, TAG, "Failed create sr data");
 
-//     g_sr_data->result_que = xQueueCreate(3, sizeof(sr_result_t));
-//     ESP_GOTO_ON_FALSE(NULL != g_sr_data->result_que, ESP_ERR_NO_MEM, err, TAG, "Failed create result queue");
+    g_sr_data->result_que = xQueueCreate(3, sizeof(sr_result_t));
+    ESP_GOTO_ON_FALSE(NULL != g_sr_data->result_que, ESP_ERR_NO_MEM, err, TAG, "Failed create result queue");
 
-//     g_sr_data->event_group = xEventGroupCreate();
-//     ESP_GOTO_ON_FALSE(NULL != g_sr_data->event_group, ESP_ERR_NO_MEM, err, TAG, "Failed create event_group");
+    g_sr_data->event_group = xEventGroupCreate();
+    ESP_GOTO_ON_FALSE(NULL != g_sr_data->event_group, ESP_ERR_NO_MEM, err, TAG, "Failed create event_group");
 
-//     SLIST_INIT(&g_sr_data->cmd_list);
+    SLIST_INIT(&g_sr_data->cmd_list);
 
-//     /* Create file if record to SD card enabled*/
-//     g_sr_data->b_record_en = record_en;
-//     if (record_en) {
-//         char file_name[32];
-//         for (size_t i = 0; i < 100; i++) {
-//             sprintf(file_name, "/sdcard/Record_%02d.pcm", i);
-//             g_sr_data->fp = fopen(file_name, "r");
-//             fclose(g_sr_data->fp);
-//             if (NULL == g_sr_data->fp) {
-//                 break;
-//             }
-//         }
-//         g_sr_data->fp = fopen(file_name, "w");
-//         ESP_GOTO_ON_FALSE(NULL != g_sr_data->fp, ESP_FAIL, err, TAG, "Failed create record file");
-//         ESP_LOGI(TAG, "File created at %s", file_name);
-//     }
+    /* Create file if record to SD card enabled*/
+    g_sr_data->b_record_en = record_en;
+    // if (record_en) {
+    //     char file_name[32];
+    //     for (size_t i = 0; i < 100; i++) {
+    //         sprintf(file_name, "/sdcard/Record_%02d.pcm", i);
+    //         g_sr_data->fp = fopen(file_name, "r");
+    //         fclose(g_sr_data->fp);
+    //         if (NULL == g_sr_data->fp) {
+    //             break;
+    //         }
+    //     }
+    //     g_sr_data->fp = fopen(file_name, "w");
+    //     ESP_GOTO_ON_FALSE(NULL != g_sr_data->fp, ESP_FAIL, err, TAG, "Failed create record file");
+    //     ESP_LOGI(TAG, "File created at %s", file_name);
+    // }
 
-//     BaseType_t ret_val;
+    BaseType_t ret_val;
 
-//     models = esp_srmodel_init("model");
-//     afe_handle = (esp_afe_sr_iface_t *)&ESP_AFE_SR_HANDLE;
-//     afe_config_t afe_config = AFE_CONFIG_DEFAULT();
+    models = esp_srmodel_init("model");
+    afe_handle = (esp_afe_sr_iface_t *)&ESP_AFE_SR_HANDLE;
+    afe_config_t afe_config = AFE_CONFIG_DEFAULT();
 
-//     afe_config.wakenet_model_name = esp_srmodel_filter(models, ESP_WN_PREFIX, NULL);
-//     afe_config.aec_init = false;
+    afe_config.wakenet_model_name = esp_srmodel_filter(models, ESP_WN_PREFIX, NULL);
+    afe_config.aec_init = false;
 
-//     esp_afe_sr_data_t *afe_data = afe_handle->create_from_config(&afe_config);
-//     g_sr_data->afe_handle = afe_handle;
-//     g_sr_data->afe_data = afe_data;
+    esp_afe_sr_data_t *afe_data = afe_handle->create_from_config(&afe_config);
+    g_sr_data->afe_handle = afe_handle;
+    g_sr_data->afe_data = afe_data;
 
-//     sys_param_t *param = settings_get_parameter();
-//     g_sr_data->lang = SR_LANG_MAX;
-//     ret = app_sr_set_language(param->sr_lang);
-//     ESP_GOTO_ON_FALSE(ESP_OK == ret, ESP_FAIL, err, TAG,  "Failed to set language");
+    sys_param_t *param = settings_get_parameter();
+    g_sr_data->lang = SR_LANG_MAX;
+    ret = app_sr_set_language(param->sr_lang);
+    ESP_GOTO_ON_FALSE(ESP_OK == ret, ESP_FAIL, err, TAG,  "Failed to set language");
 
-//     ret_val = xTaskCreatePinnedToCore(&audio_feed_task, "Feed Task", 4 * 1024, (void *)afe_data, 5, &g_sr_data->feed_task, 0);
-//     ESP_GOTO_ON_FALSE(pdPASS == ret_val, ESP_FAIL, err, TAG,  "Failed create audio feed task");
+    ret_val = xTaskCreatePinnedToCore(&audio_feed_task, "Feed Task", 4 * 1024, (void *)afe_data, 5, &g_sr_data->feed_task, 0);
+    ESP_GOTO_ON_FALSE(pdPASS == ret_val, ESP_FAIL, err, TAG,  "Failed create audio feed task");
 
-//     ret_val = xTaskCreatePinnedToCore(&audio_detect_task, "Detect Task", 8 * 1024, (void *)afe_data, 5, &g_sr_data->detect_task, 1);
-//     ESP_GOTO_ON_FALSE(pdPASS == ret_val, ESP_FAIL, err, TAG,  "Failed create audio detect task");
+    ret_val = xTaskCreatePinnedToCore(&audio_detect_task, "Detect Task", 8 * 1024, (void *)afe_data, 5, &g_sr_data->detect_task, 1);
+    ESP_GOTO_ON_FALSE(pdPASS == ret_val, ESP_FAIL, err, TAG,  "Failed create audio detect task");
 
-//     ret_val = xTaskCreatePinnedToCore(&sr_handler_task, "SR Handler Task", 6 * 1024, NULL, configMAX_PRIORITIES - 1, &g_sr_data->handle_task, 0);
-//     ESP_GOTO_ON_FALSE(pdPASS == ret_val, ESP_FAIL, err, TAG,  "Failed create audio handler task");
+    ret_val = xTaskCreatePinnedToCore(&sr_handler_task, "SR Handler Task", 6 * 1024, NULL, configMAX_PRIORITIES - 1, &g_sr_data->handle_task, 0);
+    ESP_GOTO_ON_FALSE(pdPASS == ret_val, ESP_FAIL, err, TAG,  "Failed create audio handler task");
 
-//     return ESP_OK;
-// err:
-//     app_sr_stop();
-//     return ret;
+    return ESP_OK;
+err:
+    app_sr_stop();
+    return ret;
     return 0;
 }
 
--- examples/image_display/main/image_display.c
+++ examples/image_display/main/image_display.c
@@ -15,6 +15,8 @@
 #include "file_iterator.h"
 #include "audio_player.h"
 #include "settings.h"
+#include "nvs_flash.h"
+#include "nvs.h"
 
 static const char *TAG = "main";
 
@@ -63,6 +65,15 @@ void sr_init(void)
 
 void app_main(void)
 {
+
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+    ESP_ERROR_CHECK(settings_read_parameter_from_nvs());
+
     /* Initialize I2C (for touch and audio) */
     bsp_i2c_init();
 
